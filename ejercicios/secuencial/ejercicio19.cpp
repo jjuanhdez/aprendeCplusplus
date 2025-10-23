3f63cbee702d2152acfca047e1e816ed7d1878cd
J.Juan Hdez <32603609+jjuanhdez@users.noreply.github.com>
2025-10-23 11:08:11 +0200

    Correccion

--- ejercicios/secuencial/ejercicio19.cpp
+++ ejercicios/secuencial/ejercicio19.cpp
@@ -3,16 +3,16 @@
 //por cada respuesta correcta 5 puntos, por una incorrecta -1 y por respuestas en 
 //blanco 0. Imprimir el resultado obtenido por el estudiante.
 //################################################################################
-//Análisis
+//AnÃ¡lisis
 //Se piden la cantidad de respuestas correctas, incorrectas. Calculamos la nota:
 // 5 puntos por respuesta correcta, -1 por incorrecta.
 //Datos de entrada: respuesta correctas, incorrectas y en blanco(Entero).
-//Información de salida: puntos (Entero)
+//InformaciÃ³n de salida: puntos (Entero)
 //Variables: correctas, incorrectas, puntos (Entero)
 //################################################################################
-//Diseño
+//DiseÃ±o
 //1. Leer preguntas correctas, incorrectas y en blanco
-//2. Calcular nota: número de correctas * 5 + número de incorrectas * -1
+//2. Calcular nota: nÃºmero de correctas * 5 + nÃºmero de incorrectas * -1
 //3. Mostrar puntos
 //################################################################################
 
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 	cin >> correctas;
 	cout << "Dime cantidad de respuestas incorrectas: ";
 	cin >> incorrectas;
-	puntos = correctas * 5 + incorrectas * (-1);
+	puntos = correctas * 5 - incorrectas;
 	cout << "Puntos: " << puntos << endl;
 	return 0;
 }
